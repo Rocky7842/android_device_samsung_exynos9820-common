96f53b83776fb1f7c982e985120d665bc24fae08
Peter Kalauskas <peskal@google.com>
2024-04-10 12:26:11 +0800

    exynos9820-common: ScreenOffUdfps: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- ScreenOffUdfps/src/org/lineageos/screenoffudfps/SettingsActivity.java
+++ ScreenOffUdfps/src/org/lineageos/screenoffudfps/SettingsActivity.java
@@ -3,7 +3,6 @@ package org.lineageos.screenoffudfps;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class SettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -12,7 +11,8 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(
+                com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new SettingsFragment(), TAG_SCREENOFFUDFPS).commit();
     }
 }
